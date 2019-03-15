63d08bbe723a0a2f354a7d5ae1fb00dbccb00503
Jose Duarte <dumejo92@gmail.com>
2019-03-14 21:52:25 -0600

    Updates

---
+++ precache-manifest.438826a33567dbd43c15a5f8ab39f0e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d0584dfc92c96667f47",
+    "url": "/beca-kodemia.git/static/css/main.addf4f51.chunk.css"
+  },
+  {
+    "revision": "0d0584dfc92c96667f47",
+    "url": "/beca-kodemia.git/static/js/main.6ba07164.chunk.js"
+  },
+  {
+    "revision": "a1523123f3af034eabe2",
+    "url": "/beca-kodemia.git/static/js/runtime~main.95087065.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/beca-kodemia.git/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "a384f10df30d16ad2a99675d8d2a0452",
+    "url": "/beca-kodemia.git/index.html"
+  }
+];
